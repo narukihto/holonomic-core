a8af9cb939dc9bb1ac30650e6ce0b174dddd51f6
Issac Andrew | Protocol Architect <Issaclex@proton.me>
2026-05-30 12:18:26 +0000

    Update tension.rs

--- src/core/tension.rs
+++ src/core/tension.rs
@@ -30,9 +30,7 @@ impl TensionMatrix {
                     let dist = &self.data[i][j];
                     let dist_sq: Float = dist.clone().pow(2);
                     let sigma_sq: Float = sigma.clone().pow(2);
-                    
                     let mut denom = Float::with_val(128, &two * &sigma_sq);
-                    
                     let mut exponent: Float = dist_sq;
                     exponent /= &denom;
                     exponent = -exponent;
