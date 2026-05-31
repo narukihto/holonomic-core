0257c79ae5b9fae95958fbfea79afc30248d3323
Issac Andrew | Protocol Architect <Issaclex@proton.me>
2026-05-31 18:48:24 +0000

    Update lwe_compliance_test.rs

--- examples/lwe_compliance_test.rs
+++ examples/lwe_compliance_test.rs
@@ -1,6 +1,7 @@
 use ark_penta_v_core::core::tension::TensionMatrix;
 use ark_penta_v_core::crypto::lwe::sign_manifold_async;
 use tokio::sync::mpsc;
+use std::sync::Arc;
 
 fn main() {
     let rt = tokio::runtime::Builder::new_current_thread()
@@ -10,10 +11,13 @@ fn main() {
 
     rt.block_on(async {
         let raw_data = vec![vec![1.0, 0.5], vec![0.5, 1.0]];
-        let matrix = TensionMatrix::new(raw_data);
+        let matrix = Arc::new(TensionMatrix::new(raw_data));
         let (tx, mut rx) = mpsc::channel(1);
 
-        tokio::spawn(sign_manifold_async(&matrix, tx));
+        let matrix_clone = Arc::clone(&matrix);
+        tokio::spawn(async move {
+            sign_manifold_async(&matrix_clone, tx).await;
+        });
 
         if let Some(signature) = rx.recv().await {
             if signature.is_valid {
