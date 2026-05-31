1f9ed5110798aeaadf26613e5452bebddd9b9894
Issac Andrew | Protocol Architect <Issaclex@proton.me>
2026-05-31 18:23:13 +0000

    Update holonomic_stress_tests.rs

--- tests/holonomic_stress_tests.rs
+++ tests/holonomic_stress_tests.rs
@@ -5,7 +5,12 @@ fn test_sovereign_collapse_stress_load() {
     init_sovereign_core();
 
     for i in 0..1000 {
-        let nodes: &[[f64; 2]] = &[[0.0, 0.0], [1.0, i as f64], [5.0, 2.0], [3.0, 0.0]];
+        let nodes: &[[f64; 2]] = &[
+            [0.0, 0.0],
+            [1.0, i as f64],
+            [5.0, 2.0],
+            [3.0, 0.0],
+        ];
 
         let config = QuantumBundleConfig {
             distance_matrix: vec![],
