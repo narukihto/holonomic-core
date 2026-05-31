57b12f2901a1390e49d5fee8fe39c4bf5abe1921
Issac Andrew | Protocol Architect <Issaclex@proton.me>
2026-05-31 19:31:48 +0000

    Create manifold_bench.rs

---
+++ benches/manifold_bench.rs
@@ -0,0 +1,17 @@
+use criterion::{black_box, criterion_group, criterion_main, Criterion};
+use ark_penta_v_core::{QuantumBundleConfig, SovereignManifold, execute_sovereign_collapse};
+
+fn bench_sovereign_collapse(c: &mut Criterion) {
+    let nodes: Vec<[f64; 2]> = vec![[0.0, 0.0], [1.0, 5.0], [2.0, 2.0], [5.0, 1.0], [10.0, 10.0]];
+    let config = QuantumBundleConfig {
+        distance_matrix: vec![],
+        adiabatic_time: 100.0,
+    };
+
+    c.bench_function("sovereign_collapse_5_nodes", |b| {
+        b.iter(|| execute_sovereign_collapse(black_box(&config), black_box(&nodes)))
+    });
+}
+
+criterion_group!(benches, bench_sovereign_collapse);
+criterion_main!(benches);
