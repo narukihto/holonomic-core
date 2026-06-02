a1488b5503fc087d186a0558fcda8ceeeb601c88
Issac Andrew | Protocol Architect <Issaclex@proton.me>
2026-06-02 14:58:02 +0000

    Update lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -12,10 +12,6 @@ pub use crate::physics::calibrate_resonance_lattice;
 
 pub fn execute_sovereign_collapse(config: QuantumBundleConfig, nodes: &[[f64; 2]]) -> f64 {
     let manifold = SovereignManifold::new(nodes);
-    let mut tension = manifold.compute_tension_matrix();
-
-    tension.enforce_terminal_boundary(config.adiabatic_time);
-
     config.execute_sovereign_collapse(&manifold)
 }
 
