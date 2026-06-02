8ba191b440b5f8c82668d24371063c4952ca5b82
Issac Andrew | Protocol Architect <Issaclex@proton.me>
2026-06-02 14:54:33 +0000

    Update tension.rs

--- src/core/tension.rs
+++ src/core/tension.rs
@@ -9,25 +9,32 @@ pub struct TensionMatrix {
 impl TensionMatrix {
     pub fn new(matrix: Vec<Vec<f64>>) -> Self {
         let size = matrix.len();
-        let mut data = vec![vec![Float::with_val(128, 0.0); size]; size];
-        for (i, row) in matrix.iter().enumerate() {
-            for (j, &val) in row.iter().enumerate() {
-                data[i][j] = Float::with_val(128, val);
-            }
-        }
+        let data: Vec<Vec<Float>> = matrix
+            .into_iter()
+            .map(|row| {
+                row.into_iter()
+                    .map(|val| Float::with_val(128, val))
+                    .collect()
+            })
+            .collect();
+
         Self { data, size }
     }
 
     pub fn enforce_terminal_boundary(&mut self, _t_max: f64) {
         let decay_factor = Float::with_val(128, (-10.0f64).exp());
-        for i in 0..self.size {
-            for j in 0..self.size {
-                self.data[i][j] *= &decay_factor;
+        for row in self.data.iter_mut() {
+            for val in row.iter_mut() {
+                if *val != 0.0 {
+                    *val *= &decay_factor;
+                }
             }
         }
     }
 
     pub fn apply_asymmetric_bias(&mut self, i: usize, j: usize, bias: f64) {
-        self.data[i][j] *= Float::with_val(128, bias);
+        if i < self.size && j < self.data[i].len() {
+            self.data[i][j] *= Float::with_val(128, bias);
+        }
     }
 }
