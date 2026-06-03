e2c7241bddf8b3f0a8cd70ff9ea47d24250b62a3
Issac Andrew | Protocol Architect <Issaclex@proton.me>
2026-06-03 08:23:48 +0000

    Update manifold_bench.rs

--- benches/manifold_bench.rs
+++ benches/manifold_bench.rs
@@ -4,6 +4,8 @@ use std::env;
 use std::time::Duration;
 
 fn bench_sovereign_collapse(c: &mut Criterion) {
+    let is_ci = env::var("CI").is_ok();
+
     let nodes: Vec<[f64; 2]> = vec![[0.0, 0.0], [1.0, 5.0], [2.0, 2.0], [5.0, 1.0], [10.0, 10.0]];
     let manifold = SovereignManifold::new(&nodes);
     let config = QuantumBundleConfig {
@@ -15,17 +17,21 @@ fn bench_sovereign_collapse(c: &mut Criterion) {
         b.iter(|| config.execute_sovereign_collapse(black_box(&manifold)))
     });
 
-    let large_nodes: Vec<[f64; 2]> = (0..10000).map(|i| [i as f64, (i % 100) as f64]).collect();
+    let node_count = if is_ci { 100 } else { 10000 };
+    let large_nodes: Vec<[f64; 2]> = (0..node_count)
+        .map(|i| [i as f64, (i % 100) as f64])
+        .collect();
+
     let large_manifold = SovereignManifold::new(&large_nodes);
     let tension = large_manifold.compute_tension_matrix();
 
     let mut group = c.benchmark_group("jacobian");
-    if env::var("CI").is_ok() {
+    if is_ci {
         group.sample_size(10);
-        group.measurement_time(Duration::from_secs(60));
+        group.measurement_time(Duration::from_secs(30));
     }
 
-    group.bench_function("jacobian_collapse_10000_nodes", |b| {
+    group.bench_function(format!("jacobian_collapse_{}_nodes", node_count), |b| {
         b.iter(|| collapse_to_optimum(black_box(tension.clone())))
     });
     group.finish();
