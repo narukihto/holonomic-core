c2854090932b47bdd51f5464fc3d06258fcd8fb2
Issac Andrew | Protocol Architect <Issaclex@proton.me>
2026-06-03 07:23:35 +0000

    Update manifold.rs

--- src/core/manifold.rs
+++ src/core/manifold.rs
@@ -61,7 +61,9 @@ impl SovereignManifold {
                     .collect();
 
                 if k_neighbors > 0 && k_neighbors < n {
-                    dists.select_nth_unstable_by(k_neighbors, |a, b| a.1.partial_cmp(&b.1).unwrap());
+                    dists.select_nth_unstable_by(k_neighbors, |a, b| {
+                        a.1.partial_cmp(&b.1).unwrap()
+                    });
                 }
 
                 let mut row = vec![0.0; n];
@@ -105,11 +107,11 @@ impl SovereignManifold {
             })
             .collect();
 
-       if k_neighbors > 0 && k_neighbors < n {
-                    dists.select_nth_unstable_by(k_neighbors, |a, b| {
-                        a.1.partial_cmp(&b.1).unwrap()
-                    });
-                }
+        if k_neighbors > 0 && k_neighbors < n {
+            local_nodes.select_nth_unstable_by(k_neighbors, |a, b| {
+                a.1.partial_cmp(&b.1).unwrap()
+            });
+        }
 
         for &(idx, dist_sq) in local_nodes.iter().take(k_neighbors) {
             let other = self.nodes[idx];
