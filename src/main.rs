92693e34e87bb0bbcb236b66347d968306cd2e2c
Issac Andrew | Protocol Architect <Issaclex@proton.me>
2026-06-04 01:48:19 +0000

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -7,9 +7,7 @@ fn main() {
 
     let nodes: &[[f64; 2]] = &[[0.0, 0.0], [1.0, 5.0], [5.0, 2.0], [3.0, 0.0]];
 
-    let config = QuantumBundleConfig {
-        scale: 1.0,
-    };
+    let config = QuantumBundleConfig { scale: 1.0 };
 
     println!("📐 Folding manifold for {} nodes...", nodes.len());
 
