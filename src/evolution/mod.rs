248e14bde378161d4abadeafa41492cca2aa14e5
Issac Andrew | Protocol Architect <Issaclex@proton.me>
2026-06-04 04:39:23 +0000

    Update mod.rs

--- src/evolution/mod.rs
+++ src/evolution/mod.rs
@@ -6,47 +6,39 @@ pub fn collapse_to_optimum(tension: TensionMatrix) -> Vec<usize> {
         return (0..n).collect();
     }
 
-    let mut current_path = vec![0];
-    let mut visited = vec![false; n];
-    visited[0] = true;
+    let mut current_path: Vec<usize> = (0..n).collect();
+    
+    let mut improved = true;
+    let mut iterations = 0;
+    
+    while improved && iterations < 20 {
+        improved = false;
+        iterations += 1;
+        
+        for i in 0..n - 3 {
+            for j in i + 2..n - 1 {
+                for k in j + 2..n {
+                    let a = current_path[i];
+                    let b = current_path[i + 1];
+                    let c = current_path[j];
+                    let d = current_path[j + 1];
+                    let e = current_path[k];
+                    let f = current_path[(k + 1) % n];
 
-    for _ in 1..n {
-        let last = *current_path.last().unwrap();
-        let next = (0..n)
-            .filter(|&i| !visited[i])
-            .min_by(|&a, &b| {
-                tension.data[last][a]
-                    .partial_cmp(&tension.data[last][b])
-                    .unwrap()
-            })
-            .unwrap();
-        current_path.push(next);
-        visited[next] = true;
-    }
+                    let old_dist = tension.data[a][b] + tension.data[c][d] + tension.data[e][f];
+                    let new_dist = tension.data[a][c] + tension.data[b][e] + tension.data[d][f];
 
-    let max_epochs = 100;
-    for _ in 0..max_epochs {
-        let mut improved = false;
-        for i in 0..n - 1 {
-            let next_i = (i + 1) % n;
-            let limit = (i + 500).min(n);
-            for j in i + 2..limit {
-                let next_j = (j + 1) % n;
-                let u = current_path[i];
-                let v = current_path[next_i];
-                let x = current_path[j];
-                let y = current_path[next_j];
-
-                if (tension.data[u][v] + tension.data[x][y])
-                    > (tension.data[u][x] + tension.data[v][y])
-                {
-                    current_path[next_i..=j].reverse();
-                    improved = true;
+                    if new_dist < old_dist {
+                        let mut new_segment = Vec::new();
+                        new_segment.extend_from_slice(&current_path[i + 1..=j]);
+                        new_segment.reverse();
+                        current_path[i + 1..=j].copy_from_slice(&new_segment);
+                        improved = true;
+                    }
                 }
+                if improved { break; }
             }
-        }
-        if !improved {
-            break;
+            if improved { break; }
         }
     }
     current_path
