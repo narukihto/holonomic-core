ecd9e4c1f8a9bdfec253c6e936bf5a24d11c067c
Issac Andrew | Protocol Architect <Issaclex@proton.me>
2026-06-04 19:36:11 +0000

    Update mod.rs

--- src/evolution/mod.rs
+++ src/evolution/mod.rs
@@ -1,4 +1,5 @@
 use crate::core::tension::TensionMatrix;
+use rand::{rngs::StdRng, seq::SliceRandom, SeedableRng};
 
 pub fn collapse_to_optimum(tension: TensionMatrix) -> Vec<usize> {
     let n = tension.size;
@@ -6,50 +7,28 @@ pub fn collapse_to_optimum(tension: TensionMatrix) -> Vec<usize> {
         return (0..n).collect();
     }
 
-    let mut current_path = vec![0];
-    let mut visited = vec![false; n];
-    visited[0] = true;
-    let mut last = 0;
-
-    for _ in 1..n {
-        let mut nearest = None;
-        let mut min_dist = f64::MAX;
-
-        for (next, &is_visited) in visited.iter().enumerate() {
-            if !is_visited && tension.data[last][next] < min_dist {
-                min_dist = tension.data[last][next];
-                nearest = Some(next);
-            }
-        }
-
-        if let Some(next) = nearest {
-            current_path.push(next);
-            visited[next] = true;
-            last = next;
-        }
-    }
+    let mut rng = StdRng::seed_from_u64(42);
+    let mut path: Vec<usize> = (0..n).collect();
+    path.shuffle(&mut rng);
 
     for _ in 0..5 {
         let mut improved = false;
         for i in 0..n - 3 {
             let next_i = i + 1;
-            for j in i + 2..i + 50 {
-                let k = j % n;
-                let next_j = (k + 1) % n;
+            let j = i + 2;
+            let k = i + 3;
 
-                if (tension.data[current_path[i]][current_path[next_i]]
-                    + tension.data[current_path[k]][current_path[next_j]])
-                    > (tension.data[current_path[i]][current_path[k]]
-                        + tension.data[current_path[next_i]][current_path[next_j]])
-                {
-                    current_path[next_i..=k].reverse();
-                    improved = true;
-                }
+            let current_dist = tension.data[path[i]][path[next_i]] + tension.data[path[j]][path[k]];
+            let new_dist = tension.data[path[i]][path[j]] + tension.data[path[next_i]][path[k]];
+
+            if new_dist < current_dist {
+                path.swap(next_i, j);
+                improved = true;
             }
         }
         if !improved {
             break;
         }
     }
-    current_path
+    path
 }
